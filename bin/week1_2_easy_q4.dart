c2055121aae058b44334c662ff1a29e888a9ec8d
yousefi2002 <naiebyousefihamed@gmailcom>
2025-02-17 16:12:41 +0430

    problem of question 4 is solved

--- bin/week1_2_easy_q4.dart
+++ bin/week1_2_easy_q4.dart
@@ -1,22 +1,19 @@
 import 'dart:io';
 // ---------------------------- leap year checker --------------------------- //
 
-main(){
+main() {
+  try {
+    print('Enter a year to check out is it leap or normal year.');
+    stdout.write('#_ Year: ');
 
- try{
-   print('Enter a year to check out is it leap or normal year.');
-   stdout.write('#_ Year: ');
+    int? year = int.tryParse(stdin.readLineSync() ?? '0');
 
-   int? year = int.tryParse(stdin.readLineSync() ?? '0');
-
-   if (year! % 4 == 0){
-     print('$year is a leap year 🎉');
-
-   }else{
-     print('$year is not a leap year 😒');
-
-   }
- }catch(e){
-   print('You have entered non valid year!');
- }
+    if (year! % 4 == 0  && ( year % 400 == 0 || year % 100 != 0)) {
+      print('$year is a leap year 🎉');
+    } else {
+      print('$year is not a leap year 😒');
+    }
+  } catch (e) {
+    print('You have entered non valid year!');
+  }
 }
