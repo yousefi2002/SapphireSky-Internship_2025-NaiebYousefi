34181268ebd6a6635ab03e63112493baec57cb23
yousefi2002 <naiebyousefihamed@gmailcom>
2025-02-19 15:02:16 +0430

    The answer to this comment: You can a function for checking and after printing the result, you can ask user for next input

--- bin/if_conditionals_practical_exercises/week1_2_easy_q1.dart
+++ bin/if_conditionals_practical_exercises/week1_2_easy_q1.dart
@@ -1,34 +1,53 @@
+// import 'dart:io';
+// // ---------------------------- check odd or even --------------------------- //
+// main(){
+//   stdout.write('''
+//   1.  (Empty value will cause the program to finish !)
+//   2.  (Negative number will cause the program to finish !)
+//   Enter a number to check whether it is odd or even 😎:  ''');
+//   String number = stdin.readLineSync() ?? '-1';
+//   num numberIsEvenOrOdd = num.tryParse(number) ?? -1;
+//   while(!numberIsEvenOrOdd.isNegative) {
+//
+//     if (numberIsEvenOrOdd % 2 == 0){
+//       print('The number $numberIsEvenOrOdd is Even.');
+//       print('<------------------------------------------------->');
+//       stdout.write('Enter a number to check whether it is odd or even 😎:  ');
+//       number = stdin.readLineSync() ?? '-1';
+//       numberIsEvenOrOdd = num.tryParse(number) ?? -1;
+//
+//     }else if (numberIsEvenOrOdd % 2 == 1){
+//       print('The number $numberIsEvenOrOdd is Odd.');
+//       print('<------------------------------------------------->');
+//       stdout.write('Enter a number to check whether it is odd or even 😎:  ');
+//       number = stdin.readLineSync()  ?? '-1';
+//       numberIsEvenOrOdd = num.tryParse(number) ?? -1;
+//
+//     } else{
+//       print('<------------------------------------------------->');
+//       stdout.write('Pleas enter a real number 😎: ');
+//       number = stdin.readLineSync() ?? '-1';
+//       numberIsEvenOrOdd = num.tryParse(number) ?? -1;
+//     }
+//   }
+//   print('Program Finished');
+// }
 import 'dart:io';
-// ---------------------------- check odd or even --------------------------- //
-main(){
-  stdout.write('''
-  1.  (Empty value will cause the program to finish !)
-  2.  (Negative number will cause the program to finish !)
-  Enter a number to check whether it is odd or even 😎:  ''');
-  String number = stdin.readLineSync() ?? '-1';
-  num numberIsEvenOrOdd = num.tryParse(number) ?? -1;
-  while(!numberIsEvenOrOdd.isNegative) {
 
-    if (numberIsEvenOrOdd % 2 == 0){
-      print('The number $numberIsEvenOrOdd is Even.');
-      print('<------------------------------------------------->');
-      stdout.write('Enter a number to check whether it is odd or even 😎:  ');
-      number = stdin.readLineSync() ?? '-1';
-      numberIsEvenOrOdd = num.tryParse(number) ?? -1;
+void checkOddEven() {
+  while (true) {
+    stdout.write('\nEnter a number (empty/negative to exit): ');
+    String input = stdin.readLineSync() ?? '';
+    int? number = int.tryParse(input);
 
-    }else if (numberIsEvenOrOdd % 2 == 1){
-      print('The number $numberIsEvenOrOdd is Odd.');
-      print('<------------------------------------------------->');
-      stdout.write('Enter a number to check whether it is odd or even 😎:  ');
-      number = stdin.readLineSync()  ?? '-1';
-      numberIsEvenOrOdd = num.tryParse(number) ?? -1;
+    if (number == null || number < 0) break;
 
-    } else{
-      print('<------------------------------------------------->');
-      stdout.write('Pleas enter a real number 😎: ');
-      number = stdin.readLineSync() ?? '-1';
-      numberIsEvenOrOdd = num.tryParse(number) ?? -1;
-    }
+    print('The number $number is ${number % 2 == 0 ? 'Even' : 'Odd'}.');
+    print('<------------------------------------------------->');
   }
   print('Program Finished');
+}
+
+void main() {
+  checkOddEven();
 }
