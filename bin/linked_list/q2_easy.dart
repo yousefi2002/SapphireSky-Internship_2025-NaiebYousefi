f9c26c45eff9481e58f7398838f5af2364d6d772
yousefi2002 <naiebyousefihamed@gmailcom>
2025-03-05 15:42:02 +0430

    .

--- bin/linked_list/q2_easy.dart
+++ bin/linked_list/q2_easy.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
 
 import 'node.dart';
+
 class LinkedList {
   Node? head;
 
-  // Implement a function to insert a node at the beginning of a linked list.
+  // 2.Implement a function to insert a node at the beginning of a linked list.
   void insertAtBeginning(int data) {
     Node newNode = Node(data);
     newNode.next = head;
     head = newNode;
   }
 
-  // How do you delete a node from a linked list?
+  // 3.How do you delete a node from a linked list?
   void deleteNode(int data) {
     if (head == null) return;
 
@@ -30,7 +31,7 @@ class LinkedList {
     }
   }
 
-// Write a function to traverse and print a linked list.
+// 4.Write a function to traverse and print a linked list.
   void display() {
     Node? temp = head;
     while (temp != null) {
@@ -40,6 +41,7 @@ class LinkedList {
     print('null');
   }
 
+// 5.Implement a function to find the length of a linked list.
   displayLength() {
     int count = 0;
     Node? temp = head;
@@ -49,19 +51,61 @@ class LinkedList {
     }
     return count;
   }
+
+  //
+  // middleNode(){
+  //   int maxLength = displayLength();
+  //   int count = 1;
+  //   Node? temp = head;
+  //   while (temp != null ) {
+  //     if((maxLength ~/ 2) == count){
+  //       // print(maxLength ~/ 2);
+  //       return temp.data;
+  //     }else{
+  //       temp = temp.next;
+  //       count++;
+  //     }
+  //   }
+  // }
+
+  // 6. Write a function to find the middle node of a linked list.
+  Node? middleNode() {
+    Node? slow = head, fast = head;
+
+    while (fast != null && fast.next != null) {
+      slow = slow!.next;
+      fast = fast.next!.next;
+    }
+    return slow;
+  }
+
+  // 7. Implement a function to reverse a linked list.
+  reversLinkedList() {
+    Node? pre = head;
+    Node? current = head;
+
+    while(current?.next != null){
+      pre?.next = current;
+      current = current?.next;
+  }
+    while(current?.next == null){
+      current?.next = pre;
+      pre
+    }
+  }
 }
 
-void main(){
-
+void main() {
   LinkedList linkedList = LinkedList();
   linkedList.insertAtBeginning(7);
   linkedList.insertAtBeginning(2);
   linkedList.insertAtBeginning(6);
   linkedList.insertAtBeginning(9);
+  linkedList.insertAtBeginning(1);
   linkedList.display();
-  print(linkedList.displayLength());
-  linkedList.deleteNode(2);
-  linkedList.display();
-  print(linkedList.displayLength());
-
+  // print(linkedList.displayLength());
+  // linkedList.deleteNode(2);
+  // linkedList.display();
+  // print(linkedList.displayLength());
+  print("middle node: ${linkedList.middleNode()?.data}");
 }
