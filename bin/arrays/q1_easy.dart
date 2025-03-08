9839e2915b9d10580d8b8b66a5968a0980408e3c
yousefi2002 <naiebyousefihamed@gmailcom>
2025-03-08 16:09:34 +0430

    the ternary operator replaced with the if condition, and the max number started from array[0]

--- bin/arrays/q1_easy.dart
+++ bin/arrays/q1_easy.dart
@@ -1,15 +1,17 @@
 //Write a function to find the largest element in an array.
 
 largestNumber(List<int> array) {
-  int maxNumber = 0;
+  int maxNumber = array[0];
   for (int arr in array) {
-    arr > maxNumber ? maxNumber = arr : null;
+    if(arr > maxNumber){
+      maxNumber = arr;
+    }
   }
   return maxNumber;
 }
 
 void main() {
-  List<int> numbers = [0, 1, 9, 9, 4, 5, 6, 7, 8, 3, 2, 10];
+  List<int> numbers =  [-1,-2,-3,-10];;
 
   int result = largestNumber(numbers);
   print('The largest number in this array is $result');
