1912b89a133b486c401449ff174ff8c4fc380609
yousefi2002 <naiebyousefihamed@gmailcom>
2025-03-08 16:47:35 +0430

    the number of occurrences is returned

--- bin/arrays/q4_easy.dart
+++ bin/arrays/q4_easy.dart
@@ -1,7 +1,7 @@
 //Write a function to count the occurrences of a specific element in an array.
 
 
-frequencyOfNumbers(List<int> array,int target) {
+ int frequencyOfNumbers(List<int> array,int target) {
   int num = 0;
   List <int> indies = [];
   for (int i = 0; i < array.length; i++) {
@@ -10,12 +10,12 @@ frequencyOfNumbers(List<int> array,int target) {
       indies.add(i);
     }
   }
-  return '$target is found $num times in indies $indies of the array.';
+  return num;
 }
 
 void main() {
   List<int> numbers = [0, 1, 9, 4, 9, 6, 7, 9, 3, 2, 10];
   int target = 9;
-  String result = frequencyOfNumbers(numbers, target);
+  int result = frequencyOfNumbers(numbers, target);
   print(' $result');
 }
