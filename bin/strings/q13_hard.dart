8bd391b98cf3ebdf932ce165a3a44158f230479f
yousefi2002 <naiebyousefihamed@gmailcom>
2025-03-10 15:13:46 +0430

    The comment "function should return the Map only the print part is outside of function" got applied.

--- bin/strings/q13_hard.dart
+++ bin/strings/q13_hard.dart
@@ -1,21 +1,18 @@
 // Implement a function to count the number of occurrences of each character in a string.
 
 numberOfOccurrencesOfACharacter(String char1){
-  Map<String, int> firstChar = {};
+  Map<String, int> occurrences = {};
 
 
   for(int i = 0; i < char1.length; i++){
-    firstChar[char1[i]] = (firstChar[char1[i]] ?? 0) + 1;
+    occurrences[char1[i]] = (occurrences[char1[i]] ?? 0) + 1;
   }
-
-  for(final value in firstChar.entries){
-    print('${value.key}${value.value}');
-  }
+  return occurrences;
 }
 
 void main(){
   String char1 = 'afffa';
 
-  final result = numberOfOccurrencesOfACharacter(char1) ?? '';
+  final result = numberOfOccurrencesOfACharacter(char1);
   print(result);
 }
