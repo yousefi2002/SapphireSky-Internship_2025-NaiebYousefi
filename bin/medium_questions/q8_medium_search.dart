1133c9875b4a2e86931c8bc2d1bef4a2832061e2
yousefi2002 <naiebyousefihamed@gmailcom>
2025-03-24 16:04:25 +0430

    2D grid search is initiated

---
+++ bin/medium_questions/q8_medium_search.dart
@@ -0,0 +1,22 @@
+// 8. Write a function to search for a word in a 2D grid.
+
+findWord(List<List<String>> array, String word) {
+  int wordIndex = 0;
+  int n = array.length;
+  for(int i = 0; i < n; i++){
+   for(int j = 0; j < array[i].length; j++){
+
+     }
+    }
+}
+
+void main() {
+  List<List<String>> twoDGrid = [
+    ['a', 'w', 'o', 'b'],
+    ['b', 'c', 'r', 'd'],
+    ['d', 'g', 't', 'w'],
+    ['l', 'm', 'z', 'x'],
+  ];
+  String word = 'word';
+  findWord(twoDGrid, word);
+}
