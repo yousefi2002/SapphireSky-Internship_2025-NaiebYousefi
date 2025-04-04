f6506c27317cfc38bd4dc1645a1c1666ecdfd7fc
yousefi2002 <naiebyousefihamed@gmailcom>
2025-04-04 09:58:06 +0430

    comment applied: it's not working in all cases                  test with this [0, 9,9,9,9,9,9,9,9,9,9,9,9,9, 10];

--- bin/easy_questions/q4_easy_search.dart
+++ bin/easy_questions/q4_easy_search.dart
@@ -1,7 +1,28 @@
 // Write a function to find the first occurrence of an element in a sorted array.
-import 'q2_q3_easy_search.dart';
+
+int findFirstOccurrence(List<int> arr, int target) {
+  int low = 0;
+  int high = arr.length - 1;
+  int result = -1;
+
+  while (low <= high) {
+    int mid = low + ((high - low) ~/ 2);
+
+    if (arr[mid] == target) {
+      result = mid;
+      high = mid - 1;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return result;
+}
+
 void main(){
-  List<int> array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  final result = binarySearch(array, 0, array.length-1, 9);
+  List<int> array =  [0, 9,9,9,9,9,9,9,9,9,9,9,9,9, 10];
+  final result = findFirstOccurrence(array, 9);
   print(result);
 }
