eb39eedc341b0da38ce51faf5a0d435b6df6ef41
yousefi2002 <naiebyousefihamed@gmailcom>
2025-04-04 10:16:34 +0430

    comment applied: you don't need another loop here only you can check it

--- bin/hard_questions/q14_hard_search.dart
+++ bin/hard_questions/q14_hard_search.dart
@@ -1,18 +1,21 @@
 // Implement a function to find duplicate elements in an array using a search method.
 
-List<int> findDuplicateElements(List<int> array){
+List<int> findDuplicateElements(List<int> array) {
   List<int> duplicateElementsArray = [];
-  Map<int,int> hashMap = {};
-  for(int i = 0; i < array.length; i++){
-      hashMap[array[i]] = ((hashMap[array[i]] ?? 0) + 1);
-    }
-  for (var value in hashMap.entries){
-    if(value.value > 1){
-      duplicateElementsArray.add(value.key);
+  Map<int, int> hashMap = {};
+
+  for (int i = 0; i < array.length; i++) {
+    int current = array[i];
+    hashMap[current] = (hashMap[current] ?? 0) + 1;
+
+    if (hashMap[current] == 2) {
+      duplicateElementsArray.add(current);
     }
   }
+
   return duplicateElementsArray;
 }
+
 
 void main(){
  List<int> array = [5, 2, 3, 1, 4, 9, 6, 0, 7, 8, 10, 2, 5, 4];
