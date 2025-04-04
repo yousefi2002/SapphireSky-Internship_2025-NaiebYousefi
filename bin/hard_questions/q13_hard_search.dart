91d629c133023bd10957718fed4f1e3ff4981c32
yousefi2002 <naiebyousefihamed@gmailcom>
2025-04-04 10:11:05 +0430

    comment applied: compare it with Binary Search.

--- bin/hard_questions/q13_hard_search.dart
+++ bin/hard_questions/q13_hard_search.dart
@@ -1,5 +1,35 @@
 //  Implement Jump Search and compare it with Binary Search.
 
+//Binary Search:
+// Repeatedly divides the sorted array into halves to locate the target.
+// Works only on sorted arrays.
+//
+// Jump Search:
+// Jumps ahead by fixed steps and then does a linear search in a smaller block where the target may exist.
+// Also requires a sorted array.
+//
+
+//  Use Case & Efficiency
+// Binary Search:
+//
+// Most efficient for sorted arrays.
+// Requires random access (like arrays).
+// Not suitable for linked lists.
+//
+// Jump Search:
+//
+// Also works only on sorted arrays.
+// Better than linear search, but slower than binary search.
+// Sometimes used on linked lists where binary search isn't efficient due to lack of random access.
+
+
+// Feature	              Binary Search	     Jump Search
+// Requires Sorting	      ✅ Yes	           ✅ Yes
+// Time Complexity	      O(log n)	         O(√n)
+// Space Complexity	      O(1) (iterative)	 O(1)
+// Random Access Needed  	✅ Yes	           ❌ Not mandatory
+// Typical Use	          Arrays	           Arrays or linked lists
+
 import 'dart:math';
 
 jumpSearch(List<int> array, int targetValue){
